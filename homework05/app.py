5ce7d31b1ad19e00e3cf9ec24a9d326542c9fe97
Jorge Bolivar <jbolivar@utexas.edu>
2022-04-04 21:38:16 -0500

    Redis script

---
+++ homework05/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+import json
+import logging
+import redis
+
+app = Flask(__name__)
+meteorites = {}
+data_list = []
+
+@app.route('/data',methods=['POST'])
+def transfer_data():
+    """
+    This route reads meteorite landings file and sets it into the Redis container
+
+    Args: None, it is called with /data
+
+    Returns: None, it transfers the data 
+    """
+
+    rd = redis.Redis(host='172.17.0.12', port=6379)
+    logging.info("Transferring data")
+    global meteorites
+
+    with open('ML_Data_Sample.json' , 'r') as f:
+        meteorites =  json.load(f)
+    for d in meteorites['meteorite_landings']:
+        rd.set(d['id'],json.dumps(d))
+    return f'Data transferred\n'
+
+@app.route('/data', methods=['GET'])
+def get_names():
+    """
+    Iterates through the list that was turned into a json dump
+
+    Args: None, it is called with /data
+
+    Returns: None, it returns the list of names
+    """
+
+    rd = redis.Redis(host='172.17.0.12', port=6379)
+    logging.info("Gathering names")
+    global data_list
+
+    for i in range(10001, 10301, 1):
+        data_list.append(json.loads(rd.get(i)))
+    return json.dumps(data_list, indent=2)+'\n'
